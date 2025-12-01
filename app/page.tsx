e2b95bf6c6a704406780d4fedee200a978e8164f
mariuspeterg12 <marius.peterg1@gmail.com>
2025-12-01 10:47:54 +1000

    [warp]

---
+++ app/page.tsx
@@ -0,0 +1,16 @@
+import Image from "next/image";
+import ExploreBtn from "./components/ExploreBtn";
+export default function Home() {
+  return (
+    <section>
+      <h1 className="text-center">
+        The Hub for Every Dev <br /> Event You Can't Miss
+      </h1>
+      <p className="text-center mt-5">
+        Hackathons, Meetups, and Conferences, All in One Place
+      </p>
+
+      <ExploreBtn />
+    </section>
+  );
+}
